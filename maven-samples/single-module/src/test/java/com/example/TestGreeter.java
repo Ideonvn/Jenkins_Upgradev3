f7e474a8b0d2e5ec41726ec88bb67789e5be7f3a
Ideon <20361899+Ideonvn@users.noreply.github.com>
2020-01-13 21:18:39 +0200

    Custom unit test added.

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -44,4 +44,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(meh).length(), is(greaterThan(meh.length())));
   }
+
+  @Test
+  public void customSimoneGreetingMessage() {
+    String moh = "Simone";
+
+    assertThat(greeter.greet(moh).length(), is(greaterThan(moh.length())));
+  }
 }
